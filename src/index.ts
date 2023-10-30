972c81517bb157d078d717bef449ae59c18202ac
kahosan <15116355657l@gmail.com>
2023-10-30 13:19:48 +0800

    fix: cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { Hono } from 'hono';
+import { cors } from 'hono/cors';
 import { timeline } from './router';
 
 const app = new Hono();
 
 app
+  .use('*', cors())
   .route('/timeline', timeline);
 
 export default app;
